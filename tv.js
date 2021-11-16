12fe558eaf4c00a5575f5ce6a3b699ddc0483e2e
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-11-16 15:45:21 +0530

    Add files via upload

--- tv.js
+++ tv.js
@@ -14,6 +14,20 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 600, 420);
+    if (status != "")
+    {
+        for (i = 0; s < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function tv_back() {
